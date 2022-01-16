5fc2a77d874854deca5b2e242ffc37c8cbff4db6
hYdos <haydenv06@gmail.com>
2022-01-16 16:36:32 +1000

    getting there

--- src/main/resources/shaders/animated.fs
+++ src/main/resources/shaders/animated.fs
@@ -1,15 +1,15 @@
 #version 120
-#include "sampling.glh"
 
 varying vec2 texCoord0;
-varying vec3 worldPos0;
 varying mat3 tbnMatrix;
 
-uniform vec3 C_eyePos;
 uniform sampler2D diffuse;
 
+vec2 CalcParallaxTexCoords(mat3 tbnMatrix, vec2 texCoords) {
+	return texCoords.xy + (vec3(1.0, 1.0, 1.0) * tbnMatrix).xy;
+}
+
 void main() {
-	vec3 directionToEye = normalize(C_eyePos - worldPos0);
-	vec2 texCoords = CalcParallaxTexCoords(diffuse, tbnMatrix, directionToEye, texCoord0, 1, 0);
+	vec2 texCoords = CalcParallaxTexCoords(tbnMatrix, texCoord0);
 	gl_FragColor = texture2D(diffuse, texCoords);
 }
--- src/main/resources/shaders/animated.vs
+++ src/main/resources/shaders/animated.vs
@@ -8,7 +8,6 @@ in vec4 boneDataA;
 in vec4 boneDataB;
 
 out vec2 texCoord0;
-out vec3 worldPos0;
 out mat3 tbnMatrix;
 
 uniform mat4 T_model;
@@ -25,9 +24,8 @@ void main() {
 	vec4 pos = BoneTransform * vec4(position, 1.0);
 
     gl_Position = T_MVP * pos;
-    texCoord0 = texCoord; 
-    worldPos0 = (T_model * pos).xyz;
-    
+    texCoord0 = texCoord;
+
     vec3 n = normalize((T_model * vec4(normal, 0.0)).xyz);
     vec3 t = normalize((T_model * vec4(tangent, 0.0)).xyz);
     t = normalize(t - dot(t, n) * n);

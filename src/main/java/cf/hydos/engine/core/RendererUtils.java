c18419f142b2cef43e55a7c5dfc06b3ff68000ba
hYdos <haydenv06@gmail.com>
2022-01-16 23:34:21 +1000

    one more utility method moved.

--- src/main/java/cf/hydos/engine/core/RendererUtils.java
+++ src/main/java/cf/hydos/engine/core/RendererUtils.java
@@ -12,7 +12,20 @@ public class RendererUtils {
         Vector3f up = new Vector3f(2.0f * (q.x() * q.y() + q.w() * q.z()), 1.0f - 2.0f * (q.x() * q.x() + q.z() * q.z()), 2.0f * (q.y() * q.z() - q.w() * q.x()));
         Vector3f right = new Vector3f(1.0f - 2.0f * (q.y() * q.y() + q.z() * q.z()), 2.0f * (q.x() * q.y() - q.w() * q.z()), 2.0f * (q.x() * q.z() + q.w() * q.y()));
 
-        return new Matrix4f().identity().rotate(forward, up, right);
+        return rotate(new Matrix4f().identity(), forward, up, right);
+    }
+
+    public static Matrix4f rotate(Matrix4f original, Vector3f forward, Vector3f up, Vector3f right) {
+        original.m[0][0] = right.x();
+        original.m[0][1] = right.y();
+        original.m[0][2] = right.z();
+        original.m[1][0] = up.x();
+        original.m[1][1] = up.y();
+        original.m[1][2] = up.z();
+        original.m[2][0] = forward.x();
+        original.m[2][1] = forward.y();
+        original.m[2][2] = forward.z();
+        return original;
     }
 
     public static Matrix4f WeirdMul(Matrix4f left, Matrix4f right) {

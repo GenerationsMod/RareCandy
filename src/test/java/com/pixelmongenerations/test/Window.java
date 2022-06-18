a297f7474468b609fccdb468d64d76c65da4c092
hydos <haydenv06@gmail.com>
2022-06-17 23:30:28 -0700

    Support Loading OpenGL 4.5... again

--- src/test/java/com/pixelmongenerations/test/Window.java
+++ src/test/java/com/pixelmongenerations/test/Window.java
@@ -3,21 +3,21 @@ package com.pixelmongenerations.test;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL43C;
+import org.lwjgl.opengl.GL45C;
 import org.lwjgl.opengl.GLCapabilities;
 import org.lwjgl.system.MemoryUtil;
 
-import javax.swing.*;
 import java.util.Objects;
 
 /**
  * New modern window handling, so I don't cringe looking at this codebase as much.
  */
 public class Window {
-    private static final boolean DEBUGGING = true;
-
     public final String title;
     public final int width;
     public final int height;
+    public GLCapabilities gl;
     private long handle;
 
     public Window(String title, int width, int height) {
@@ -25,30 +25,7 @@ public class Window {
         this.title = title;
         this.width = width;
         this.height = height;
-
         initGlfw();
-    }
-
-    private void collectGlDiagnostics(GLCapabilities capabilities) {
-        boolean supportsLatest = capabilities.OpenGL46;
-        boolean supports45 = capabilities.OpenGL45;
-
-        /* FIXME: Make this work better. Creating capabilities with a set version causes issues because then this thinks that something isn't supported...
-                if (!supportsLatest && !supports45) {
-            popup(JOptionPane.ERROR_MESSAGE, "Your GPU does not support OpenGL 4.5 or 4.6 :(. Please try to update your GPU and CPU drivers.");
-        }
-
-        if (!supportsLatest) {
-            popup(JOptionPane.WARNING_MESSAGE, "Your GPU does not support OpenGL 4.6. We can still but run but that isn't good for the future...");
-        }*/
-    }
-
-    private void popup(int popupType, String message) {
-        JOptionPane.showMessageDialog(null, message, "Pixelmon Renderer", popupType);
-
-        if (popupType == JOptionPane.ERROR_MESSAGE) {
-            System.exit(-1);
-        }
     }
 
     private void initGlfw() {
@@ -61,9 +38,6 @@ public class Window {
 
         // Setup Window Hints
         GLFW.glfwDefaultWindowHints();
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 3);
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
         GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE);
 
         // Setup Window
@@ -78,18 +52,15 @@ public class Window {
         GLFWVidMode videoMode = Objects.requireNonNull(GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor()), "VideoMode was null");
         GLFW.glfwSetWindowPos(this.handle, (videoMode.width() - this.width) / 2, (videoMode.height() - this.height) / 2);
 
-        // Setup callbacks
-        GLFW.glfwSetWindowSizeCallback(this.handle, (pWindow, width1, height1) -> onResize());
-
         // Set the OpenGL context
         GLFW.glfwMakeContextCurrent(this.handle);
-        GLFW.glfwSwapInterval(0); // Dont limit the fps pls k tnx.
+        GLFW.glfwSwapInterval(0);
 
-        // Setup OpenGL debugging if enabled
-        collectGlDiagnostics(GL.createCapabilities(false));
-        if (DEBUGGING) {
-            //GL45C.glDebugMessageCallback(this::onGlError, MemoryUtil.NULL);
-            //GL45C.glEnable(GL45C.GL_DEBUG_OUTPUT);
+        this.gl = GL.createCapabilities(true);
+
+        if (this.gl.OpenGL45) {
+            GL45C.glDebugMessageCallback(this::onGlError, MemoryUtil.NULL);
+            GL45C.glEnable(GL45C.GL_DEBUG_OUTPUT);
         }
 
         GLFW.glfwShowWindow(this.handle);
@@ -111,11 +82,7 @@ public class Window {
         GLFW.glfwDestroyWindow(this.handle);
     }
 
-    private void onResize() {
-        //throw new RuntimeException("Resizing the window is not supported!");
-    }
-
-/*    private void onGlError(int glSource, int glType, int id, int severity, int length, long pMessage, long userParam) {
+    private void onGlError(int glSource, int glType, int id, int severity, int length, long pMessage, long userParam) {
         String source = switch (glSource) {
             case GL43C.GL_DEBUG_SOURCE_API -> "api";
             case GL43C.GL_DEBUG_SOURCE_WINDOW_SYSTEM -> "window system";
@@ -140,7 +107,7 @@ public class Window {
         if(!type.equals("'other'")) {
             System.out.println("[OpenGL " + source + " " + type + "] Message: " + MemoryUtil.memUTF8(pMessage));
         }
-    }*/
+    }
 
     private void onGlfwError(int error, long pDescription) {
         String description = MemoryUtil.memUTF8(pDescription);

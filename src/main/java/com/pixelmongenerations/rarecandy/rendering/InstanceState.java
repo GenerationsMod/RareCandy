8ef7d76efa33a5450ac6675f557d1dc906722f60
Waterpicker <waterpickerenternity@gmail.com>
2022-10-19 23:36:59 -0500

    Unshackle InstanceState from being record.

--- src/main/java/com/pixelmongenerations/rarecandy/rendering/InstanceState.java
+++ src/main/java/com/pixelmongenerations/rarecandy/rendering/InstanceState.java
@@ -2,4 +2,28 @@ package com.pixelmongenerations.rarecandy.rendering;
 
 import org.joml.Matrix4f;
 
-public record InstanceState(Matrix4f transformationMatrix, Matrix4f viewMatrix, String materialId) {}
+import java.util.Objects;
+
+public final class InstanceState {
+    private final Matrix4f transformationMatrix;
+    private final Matrix4f viewMatrix;
+    private final String materialId;
+
+    public InstanceState(Matrix4f transformationMatrix, Matrix4f viewMatrix, String materialId) {
+        this.transformationMatrix = transformationMatrix;
+        this.viewMatrix = viewMatrix;
+        this.materialId = materialId;
+    }
+
+    public Matrix4f transformationMatrix() {
+        return transformationMatrix;
+    }
+
+    public Matrix4f viewMatrix() {
+        return viewMatrix;
+    }
+
+    public String materialId() {
+        return materialId;
+    }
+}

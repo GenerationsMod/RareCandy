f60d5db653186ef8fa65024915466633a5b0b4ff
Hayden Verschelden <haydenv06@gmail.com>
2023-08-18 20:49:03 +1000

    better support for texture uploading

--- src/renderer/java/gg/generations/rarecandy/legacy/pipeline/UniformUploadContext.java
+++ src/renderer/java/gg/generations/rarecandy/legacy/pipeline/UniformUploadContext.java
@@ -2,9 +2,16 @@ package gg.generations.rarecandy.legacy.pipeline;
 
 import gg.generations.rarecandy.arceus.model.Model;
 import gg.generations.rarecandy.arceus.model.RenderingInstance;
+import gg.generations.rarecandy.legacy.model.misc.Texture;
 
 public record UniformUploadContext(
         Model object,
         RenderingInstance instance,
         Uniform uniform
-) {}
+) {
+
+    public void bindAndUploadTex(Texture texture, int slot) {
+        texture.bind(slot);
+        uniform.uploadInt(slot);
+    }
+}

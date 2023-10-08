e193b97a03338f6d298c40aed61df28cb9864cc1
Waterpicker <Waterpickerenternity@gmail.com>
2023-10-08 17:38:31 -0500

    Tweak to the culling logic. 1.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "gg.generations"
-version = "2.4.1-SNAPSHOT"
+version = "2.4.2-SNAPSHOT"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
--- src/library/java/gg/generations/rarecandy/pokeutils/CullType.java
+++ src/library/java/gg/generations/rarecandy/pokeutils/CullType.java
@@ -3,8 +3,8 @@ package gg.generations.rarecandy.pokeutils;
 import org.lwjgl.opengl.GL11;
 
 public enum CullType {
-    Back(GL11.GL_CW),
-    Forward(GL11.GL_CCW),
+    Back(GL11.GL_BACK),
+    Forward(GL11.GL_FRONT),
     None(-1);
 
     private final int glConstant;
@@ -24,14 +24,14 @@ public enum CullType {
     }
 
     public void enable() {
-        if(glConstant == -1) {
+        if(glConstant != -1) {
             GL11.glEnable(GL11.GL_CULL_FACE);
             GL11.glCullFace(glConstant);
         }
     }
 
     public void disable() {
-        if(glConstant == -1) {
+        if(glConstant != -1) {
             GL11.glDisable(GL11.GL_CULL_FACE);
         }
     }

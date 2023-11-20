3a937a8a064d1b77794a2bd1edf15e7f00fcccc6
Waterpicker <Waterpickerenternity@gmail.com>
2023-11-20 01:56:18 -0600

    gfbanimation tune up

--- src/library/java/gg/generations/rarecandy/renderer/animation/GfbAnimation.java
+++ src/library/java/gg/generations/rarecandy/renderer/animation/GfbAnimation.java
@@ -1,15 +1,27 @@
 package gg.generations.rarecandy.renderer.animation;
 
-import gg.generations.rarecandy.pokeutils.GFLib.Anim.AnimationT;
-import gg.generations.rarecandy.pokeutils.GFLib.Anim.ShaderEntryT;
+import com.google.gson.*;
+import gg.generations.rarecandy.pokeutils.GFLib.Anim.*;
+import gg.generations.rarecandy.renderer.rendering.Bone;
+import org.apache.commons.compress.utils.Sets;
 import org.joml.Vector3f;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.OptionalInt;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.stream.Stream.of;
+
 public class GfbAnimation extends Animation<AnimationT> {
+
     public TransformStorage<Float> eyeOffsetU;
     public TransformStorage<Float> eyeOffsetV;
 
@@ -27,28 +39,27 @@ public class GfbAnimation extends Animation<AnimationT> {
             }
         }
 
+        fillEyeOffset(rawAnimation);
+
         animationModifier.accept(this, "gfb");
     }
 
-    public AnimationNode[] fillAnimationNodes(AnimationT rawAnimation) {
-        var animationNodes = new AnimationNode[skeleton.boneMap.size()]; // BoneGroup
+    private void fillEyeOffset(AnimationT rawAnimation) {
+
 
         if(rawAnimation.getMaterial() != null) {
             var material = rawAnimation.getMaterial();
 
-            var map = Arrays.stream(material.getTracks()).filter(a -> a.getName().equals("Eye")).flatMap(a -> Stream.of(a.getValues())).collect(Collectors.toMap(ShaderEntryT::getName, ShaderEntryT::getValue));
-
-            var uOffset = map.get("ColorUVTranslateU");
-            var vOffset = map.get("ColorUVTranslateV");
+            var map = Arrays.stream(material.getTracks()).filter(a -> a.getName().equals("Eye")).flatMap(a -> of(a.getValues())).collect(Collectors.toMap(ShaderEntryT::getName, ShaderEntryT::getValue));
 
             eyeOffsetU = new TransformStorage<Float>();
             eyeOffsetV = new TransformStorage<Float>();
 
-            if(uOffset != null) {
-                uOffset.getValue().process(eyeOffsetU);
-            } else {
-                eyeOffsetU.add(0.0, 0.0f);
-            }
+            var uOffset = map.get("ColorUVTranslateU");
+            var vOffset = map.get("ColorUVTranslateV");
+
+            if(uOffset != null) uOffset.getValue().process(eyeOffsetU);
+            else eyeOffsetU.add(0.0, 0.0f);
 
             if(vOffset != null) {
                 vOffset.getValue().process(eyeOffsetV);
@@ -56,27 +67,21 @@ public class GfbAnimation extends Animation<AnimationT> {
                 eyeOffsetV.add(0.0, 0.0f);
             }
         }
+    }
+
+    public AnimationNode[] fillAnimationNodes(AnimationT rawAnimation) {
+
+        var animationNodes = new AnimationNode[rawAnimation.getSkeleton().getTracks().length];
 
         if (rawAnimation.getSkeleton() != null) {
-            int trueIndex = -1;
 
-            for (int i = 0; i < rawAnimation.getSkeleton().getTracks().length; i++) {
-                var boneAnim = rawAnimation.getSkeleton().getTracks()[i];
-                if (!skeleton.boneMap.containsKey(boneAnim.getName())) {
-                    continue;
-                }
+            for (var track : rawAnimation.getSkeleton().getTracks()) {
+                var node = animationNodes[nodeIdMap.computeIfAbsent(track.getName(), this::newNode)] = new AnimationNode();
 
-                trueIndex++;
-
-                nodeIdMap.put(Objects.requireNonNull(boneAnim.getName()).replace(".trmdl", ""), trueIndex);
-
-                var node = animationNodes[trueIndex] = new AnimationNode();
-
-                boneAnim.getRotate().getValue().process(node.rotationKeys);
-                boneAnim.getScale().getValue().process(node.scaleKeys);
-                boneAnim.getTranslate().getValue().process(node.positionKeys);
+                track.getRotate().getValue().process(node.rotationKeys);
+                track.getScale().getValue().process(node.scaleKeys);
+                track.getTranslate().getValue().process(node.positionKeys);
             }
-
         }
         return animationNodes;
     }

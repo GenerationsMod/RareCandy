cde192ed1f0ba95744c4b318c83811f54d770da1
Waterpicker <waterpickerenternity@gmail.com>
2024-02-26 18:43:27 -0600

    Push to maven. :3

--- src/main/java/gg/generations/rarecandy/tools/gui/VertexBoneData.java
+++ src/main/java/gg/generations/rarecandy/tools/gui/VertexBoneData.java
@@ -7,15 +7,13 @@ public record VertexBoneData(int[] ids, float[] weights) {
             this(new int[4], new float[4]);
         }
 
-        public void addBoneData(int BoneID, float Weight) {
+        public void addBoneData(int boneId, float weight) {
             for (var i = 0 ; i < ids.length; i++) {
                 if (weights[i] == 0.0) {
-                    ids[i] = BoneID;
-                    weights[i] = Weight;
+                    ids[i] = boneId;
+                    weights[i] = weight;
                     return;
                 }
             }
-
-            // should never get here - more bones than we have space for
         }
     }

832bf1e92a10e59fe99ecbd11321668d7f64384c
Waterpicker <waterpickerenternity@gmail.com>
2024-06-11 04:06:16 -0500

    STBimage png stuff!

--- src/main/java/gg/generations/rarecandy/renderer/loading/Texture.java
+++ src/main/java/gg/generations/rarecandy/renderer/loading/Texture.java
@@ -5,9 +5,9 @@ import com.traneptora.jxlatte.JXLOptions;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL11C;
 import org.lwjgl.opengl.GL13C;
+import org.lwjgl.opengl.GL30;
 import org.lwjgl.system.MemoryUtil;
 
-import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.DataBufferFloat;
@@ -16,6 +16,10 @@ import java.io.ByteArrayInputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.stb.STBImage.stbi_info_from_memory;
+import static org.lwjgl.stb.STBImage.stbi_load_from_memory;
 
 public class Texture implements ITexture {
     private static final JXLOptions options;
@@ -25,14 +29,19 @@ public class Texture implements ITexture {
         options.hdr = JXLOptions.HDR_OFF;
         options.threads = 4;
     }
+
     private TextureDetails details;
     public int id;
 
-    public Texture(ByteBuffer buffer, int width, int height) {
-        details = new TextureDetails(buffer, width, height);
+    public Texture(ByteBuffer buffer, Type type, int width, int height) {
+        this(new TextureDetails(buffer, type, width, height));
     }
 
-    private record TextureDetails(ByteBuffer buffer, int width, int height) implements Closeable {
+    public Texture(TextureDetails textureDetails) {
+        this.details = textureDetails;
+    }
+
+    private record TextureDetails(ByteBuffer buffer, Type type, int width, int height) implements Closeable {
         @Override
         public void close() {
             MemoryUtil.memFree(buffer());
@@ -41,7 +50,7 @@ public class Texture implements ITexture {
         public int init() {
             var id = GL11.glGenTextures();
             GL11C.glBindTexture(GL11C.GL_TEXTURE_2D, id);
-            GL11C.glTexImage2D(GL11C.GL_TEXTURE_2D, 0, GL11C.GL_RGBA8, width, height, 0, GL11C.GL_RGBA, GL11C.GL_UNSIGNED_BYTE, buffer);
+            GL11C.glTexImage2D(GL11C.GL_TEXTURE_2D, 0, type.internalFormat, width, height, 0, type.format, type.type, buffer);
 
             GL11C.glTexParameteri(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_WRAP_S, GL11C.GL_REPEAT);
             GL11C.glTexParameteri(GL11C.GL_TEXTURE_2D, GL11C.GL_TEXTURE_WRAP_T, GL11C.GL_REPEAT);
@@ -72,30 +81,52 @@ public class Texture implements ITexture {
     }
 
     public static Texture read(byte[] imageBytes, String name) throws IOException {
-        BufferedImage pixelData;
-        BufferedImage temp;
-
-        if (name.endsWith("jxl")) {
-            temp = new JXLDecoder(new ByteArrayInputStream(imageBytes), options).decode().asBufferedImage();
-        } else {
-
-            temp = ImageIO.read(new ByteArrayInputStream(imageBytes));
-        }
-
-        var width = temp.getWidth();
-        var height = temp.getHeight();
-        pixelData = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                int p = temp.getRGB(x, y);
-                pixelData.setRGB(x, y, p);
-            }
-        }
-
-        return new Texture(read(pixelData), pixelData.getWidth(), pixelData.getHeight());
+        TextureDetails temp;
+        if (name.endsWith("jxl"))
+            temp = read(new JXLDecoder(new ByteArrayInputStream(imageBytes), options).decode().asBufferedImage());
+        else temp = read(imageBytes);
+        return new Texture(temp);
     }
 
-    public static ByteBuffer read(BufferedImage image) {
+    public static TextureDetails read(byte[] bytes) {
+        ByteBuffer imageBuffer = MemoryUtil.memAlloc(bytes.length).put(bytes).flip();
+
+        IntBuffer wBuffer = MemoryUtil.memAllocInt(1);
+        IntBuffer hBuffer = MemoryUtil.memAllocInt(1);
+        IntBuffer compBuffer = MemoryUtil.memAllocInt(1);
+
+        // Use info to read image metadata without decoding the entire image.
+        // We don't need this for this demo, just testing the API.
+        if (!stbi_info_from_memory(imageBuffer, wBuffer, hBuffer, compBuffer)) {
+//               throw new RuntimeException("Failed to read image information: " + stbi_failure_reason());
+
+            return null;
+        }
+
+        // Decode the image
+        var image = stbi_load_from_memory(imageBuffer, wBuffer, hBuffer, compBuffer, 0);
+        if (image == null) {
+
+            return null;
+//            throw new RuntimeException("Failed to load image: " + stbi_failure_reason());
+        }
+
+        var w = wBuffer.get(0);
+        var h = hBuffer.get(0);
+        var comp = compBuffer.get(0);
+
+        MemoryUtil.memFree(wBuffer);
+        MemoryUtil.memFree(hBuffer);
+        MemoryUtil.memFree(compBuffer);
+        MemoryUtil.memFree(imageBuffer);
+
+        if (comp != 3 && comp != 4) throw new RuntimeException("Inccorect amount of color channels");
+
+
+        return new TextureDetails(image, comp == 3 ? Type.RGB_BYTE : Type.RGBA_BYTE, w, h);
+    }
+
+    public static TextureDetails read(BufferedImage image) {
         if (image == null) {
             return null;
         }
@@ -103,41 +134,92 @@ public class Texture implements ITexture {
         var buffer = image.getData().getDataBuffer();
 
         ByteBuffer readyData;
+        Type type;
 
-        if (buffer instanceof DataBufferFloat intBuffer) {
-            var rawData = intBuffer.getData();
-            readyData = MemoryUtil.memAlloc(rawData.length * 4);
+        if (buffer instanceof DataBufferFloat floatBuffer) {
+            var length = floatBuffer.getSize();
 
-            for (var hdrChannel : rawData) {
-                var channelValue = hdrToRgb(hdrChannel);
-                readyData.put((byte) channelValue);
+            var channels = floatBuffer.getBankData().length;
+
+            readyData = MemoryUtil.memAlloc(length * Float.BYTES * channels);
+            var rawData = floatBuffer.getBankData();
+
+            if(channels == 3) {
+                for (int i = 0; i < length; i++) {
+                    readyData.putFloat(rawData[0][i]);
+                    readyData.putFloat(rawData[1][i]);
+                    readyData.putFloat(rawData[2][i]);
+                }
+            } else if(channels == 4) {
+                for (int i = 0; i < length; i++) {
+                    readyData.putFloat(rawData[0][i]);
+                    readyData.putFloat(rawData[1][i]);
+                    readyData.putFloat(rawData[2][i]);
+                    readyData.putFloat(rawData[3][i]);
+                }
+            }else {
+                throw new RuntimeException("Float buffer lacks 3 or 4 banks.");
             }
 
             readyData.flip();
+
+            type = channels == 3 ? Type.RGB_FLOAT : Type.RGBA_FLOAT;
         } else if (buffer instanceof DataBufferInt floatBuffer) {
             var rawData = floatBuffer.getData();
+            var isRGBA = image.getType() == BufferedImage.TYPE_INT_ARGB;
+
             readyData = MemoryUtil.memAlloc(rawData.length * 4);
 
-            for (var pixel : rawData) {
-                readyData.put((byte) ((pixel >> 16) & 0xFF));
-                readyData.put((byte) ((pixel >> 8) & 0xFF));
-                readyData.put((byte) (pixel & 0xFF));
-                readyData.put((byte) ((pixel >> 24) & 0xFF));
+            if(isRGBA) {
+                for (var pixel : rawData) {
+                    readyData.put((byte) ((pixel >> 16) & 0xFF));
+                    readyData.put((byte) ((pixel >> 8) & 0xFF));
+                    readyData.put((byte) (pixel & 0xFF));
+                    readyData.put((byte) ((pixel >> 24) & 0xFF));
+                }
+            } else {
+                for (var pixel : rawData) {
+                    readyData.put((byte) ((pixel >> 16) & 0xFF));
+                    readyData.put((byte) ((pixel >> 8) & 0xFF));
+                    readyData.put((byte) (pixel & 0xFF));
+                }
             }
 
             readyData.flip();
+
+            type = isRGBA ? Type.RGBA_BYTE : Type.RGB_BYTE;
         } else if (buffer instanceof DataBufferByte dataBufferByte) {
             var rawData = dataBufferByte.getData();
-            readyData = MemoryUtil.memAlloc(rawData.length);
+            var isRGBA = image.getType() == BufferedImage.TYPE_INT_ARGB;
 
+            readyData = MemoryUtil.memAlloc(rawData.length);
             readyData.put(rawData);
             readyData.flip();
+
+            type = isRGBA ? Type.RGBA_BYTE : Type.RGB_BYTE;
         } else throw new RuntimeException("Unknown Data Type: " + buffer.getClass().getName());
 
-        return readyData;
+        return new TextureDetails(readyData, type, image.getWidth(), image.getHeight());
     }
 
-    private static int hdrToRgb(float hdr) {
+    private static double hdrToRgb(float hdr) {
         return (int) Math.min(Math.max(Math.pow(hdr, 1.0 / 2.2) * 255, 0), 255);
+    }
+
+    public enum Type {
+        RGBA_BYTE(GL30.GL_RGBA8, GL30.GL_RGBA, GL30.GL_UNSIGNED_BYTE),
+        RGB_BYTE(GL30.GL_RGB8, GL30.GL_RGB, GL30.GL_UNSIGNED_BYTE),
+        RGBA_FLOAT(GL30.GL_RGBA32F, GL30.GL_RGBA, GL30.GL_FLOAT),
+        RGB_FLOAT(GL30.GL_RGB32F, GL30.GL_RGB, GL30.GL_FLOAT);
+
+        private final int internalFormat;
+        private final int format;
+        private final int type;
+
+        Type(int internalFormat, int format, int type) {
+            this.internalFormat = internalFormat;
+            this.format = format;
+            this.type = type;
+        }
     }
 }

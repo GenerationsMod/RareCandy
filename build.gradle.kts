7edf921397362f6712a8d3d9c3bb7a36549bffe9
Waterpicker <waterpickerenternity@gmail.com>
2024-08-07 03:30:45 -0500

    2.9.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "gg.generations"
-version = "2.9.3"
+version = "2.9.4"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
@@ -27,44 +27,46 @@ repositories {
     mavenLocal()
     maven("https://jitpack.io")
     maven("https://maven.generations.gg/releases")
+    maven("https://mvnrepository.com/artifact/commons-io/commons-io/2.16.1")
 }
 
 dependencies {
     compileOnly("org.jetbrains:annotations:24.1.0")
 
-    shadow(implementation("org.tukaani", "xz", "1.9"))
-    shadow(implementation("org.apache.commons", "commons-compress", "1.26.1"))
-    shadow(implementation("org.joml", "joml", "1.10.5"))
-    shadow(implementation("de.javagl", "jgltf-model", "2.0.5"))
+    implementation("org.tukaani", "xz", "1.9")
+    implementation("org.apache.commons", "commons-compress", "1.21")
+    implementation("commons-io", "commons-io", "2.16.1")
+    implementation("org.joml", "joml", "1.10.5")
+    implementation("de.javagl", "jgltf-model", "2.0.5")
 
-    shadow(implementation(platform("org.lwjgl:lwjgl-bom:3.3.2"))!!)
-    shadow(implementation("org.lwjgl", "lwjgl"))
-    shadow(implementation("org.lwjgl", "lwjgl-glfw"))
-    shadow(implementation("org.lwjgl", "lwjgl-opengl"))
-    shadow(implementation("org.lwjgl", "lwjgl-stb"))
-    shadow(implementation("org.lwjgl", "lwjgl-assimp"))
-    shadow(implementation("com.github.thecodewarrior", "BinarySMD", "-SNAPSHOT"))
+    implementation(platform("org.lwjgl:lwjgl-bom:3.3.2"))
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("com.github.thecodewarrior", "BinarySMD", "-SNAPSHOT")
 
-    shadow(runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-windows"))
-    shadow(runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = "natives-windows"))
-    shadow(runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-windows"))
-    shadow(runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = "natives-windows"))
-    shadow(runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = "natives-windows"))
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", classifier = "natives-windows")
 
-    shadow(implementation("org.slf4j:slf4j-jdk14:2.0.12")!!)
+    implementation("org.slf4j:slf4j-jdk14:2.0.12")!!
 
     // PokeUtils Libs
-    shadow(implementation("com.github.weisj:darklaf-core:3.0.2")!!)
-    shadow(implementation("com.intellij:forms_rt:7.0.3")!!)
-    shadow(implementation("org.lwjgl", "lwjgl-nfd"))
-    shadow(runtimeOnly("org.lwjgl", "lwjgl-nfd", classifier = "natives-windows"))
-    shadow(implementation("org.lwjglx", "lwjgl3-awt", "0.1.8"))
+    implementation("com.github.weisj:darklaf-core:3.0.2")
+    implementation("com.intellij:forms_rt:7.0.3")
+    implementation("org.lwjgl", "lwjgl-nfd")
+    runtimeOnly("org.lwjgl", "lwjgl-nfd", classifier = "natives-windows")
+    implementation("org.lwjglx", "lwjgl3-awt", "0.1.8")
 
-    shadow(implementation("com.google.flatbuffers:flatbuffers-java:23.5.26")!!)
+    implementation("com.google.flatbuffers:flatbuffers-java:23.5.26")
 
     //TODO: JT need some funky gradle logic that lets us build a version does and doesn't include gson for the viewer and generations respectively
-    shadow(implementation("com.google.code.gson:gson:2.10.1")!!)
-    shadow(implementation("de.javagl:jgltf-model-builder:2.0.5")!!)
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("de.javagl:jgltf-model-builder:2.0.5")
 
 
 }

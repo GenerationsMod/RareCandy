512f4c679ee4ba3946a22782545fa6735d4929a4
Waterpicker <waterpickerenternity@gmail.com>
2024-08-07 03:52:47 -0500

    2.9.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "gg.generations"
-version = "2.9.4"
+version = "2.9.5"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
@@ -33,19 +33,19 @@ repositories {
 dependencies {
     compileOnly("org.jetbrains:annotations:24.1.0")
 
-    implementation("org.tukaani", "xz", "1.9")
+    implementation("org.tukaani", "xz", "1.9") { isTransitive = false }
     implementation("org.apache.commons", "commons-compress", "1.21")
     implementation("commons-io", "commons-io", "2.16.1")
     implementation("org.joml", "joml", "1.10.5")
     implementation("de.javagl", "jgltf-model", "2.0.5")
 
     implementation(platform("org.lwjgl:lwjgl-bom:3.3.2"))
-    implementation("org.lwjgl", "lwjgl")
-    implementation("org.lwjgl", "lwjgl-glfw")
-    implementation("org.lwjgl", "lwjgl-opengl")
-    implementation("org.lwjgl", "lwjgl-stb")
-    implementation("org.lwjgl", "lwjgl-assimp")
-    implementation("com.github.thecodewarrior", "BinarySMD", "-SNAPSHOT")
+    implementation("org.lwjgl", "lwjgl")  { isTransitive = false }
+    implementation("org.lwjgl", "lwjgl-glfw")  { isTransitive = false }
+    implementation("org.lwjgl", "lwjgl-opengl")  { isTransitive = false }
+    implementation("org.lwjgl", "lwjgl-stb")  { isTransitive = false }
+    implementation("org.lwjgl", "lwjgl-assimp")  { isTransitive = false }
+    implementation("com.github.thecodewarrior", "BinarySMD", "-SNAPSHOT")  { isTransitive = false }
 
     runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-windows")
     runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = "natives-windows")
@@ -56,17 +56,17 @@ dependencies {
     implementation("org.slf4j:slf4j-jdk14:2.0.12")!!
 
     // PokeUtils Libs
-    implementation("com.github.weisj:darklaf-core:3.0.2")
-    implementation("com.intellij:forms_rt:7.0.3")
-    implementation("org.lwjgl", "lwjgl-nfd")
+    implementation("com.github.weisj:darklaf-core:3.0.2") { isTransitive = false }
+    implementation("com.intellij:forms_rt:7.0.3") { isTransitive = false }
+    implementation("org.lwjgl", "lwjgl-nfd") { isTransitive = false }
     runtimeOnly("org.lwjgl", "lwjgl-nfd", classifier = "natives-windows")
-    implementation("org.lwjglx", "lwjgl3-awt", "0.1.8")
+    implementation("org.lwjglx", "lwjgl3-awt", "0.1.8") { isTransitive = false }
 
     implementation("com.google.flatbuffers:flatbuffers-java:23.5.26")
 
     //TODO: JT need some funky gradle logic that lets us build a version does and doesn't include gson for the viewer and generations respectively
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("de.javagl:jgltf-model-builder:2.0.5")
+    implementation("com.google.code.gson:gson:2.10.1")  { isTransitive = false }
+    implementation("de.javagl:jgltf-model-builder:2.0.5")  { isTransitive = false }
 
 
 }
